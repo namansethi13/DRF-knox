f7019b3195b6f91b6a505ebda5af78a941dc54db
stinger13 <93597591+stinger13@users.noreply.github.com>
2023-05-29 23:41:36 +0530

    login register logout and logoutAll setup

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from knox import views as knox_views
+from .views import RegisterAPI
+from django.urls import path
+from .views import LoginAPI , RegisterAPI , getuser
+urlpatterns = [
+    path('register', RegisterAPI.as_view(), name='register'),
+    path('login', LoginAPI.as_view(), name='login'),
+    path('logout', knox_views.LogoutView.as_view(), name='logout'),
+    path('logoutall', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    path('user', getuser, name='getuser'),
+
+]
